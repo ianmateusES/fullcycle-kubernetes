758d19396f976ea7b283529f90bc1effd03704b6
ianmateusES <estudo.ianmateus@gmail.com>
2023-12-24 01:00:30 -0300

    feat: creating configMap env

--- server.go
+++ server.go
@@ -2,12 +2,16 @@ package main
 
 import (
   "net/http"
-  "fmt"
   "os"
+  "fmt"
+  "io/ioutil"
+  "log"
 )
 
 func main() {
   http.HandleFunc("/", Hello)
+  http.HandleFunc("/configmap", ConfigMap)
+
   http.ListenAndServe(":8080", nil)
 }
 
@@ -16,4 +20,13 @@ func Hello(w http.ResponseWriter, r *http.Request) {
   age := os.Getenv("AGE")
 
   fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+  data, err := ioutil.ReadFile("/go/myfamily/family.txt")
+
+  if err != nil {
+    log.Fatalf("Error reading file: ", err)
+  }
+  fmt.Fprintf(w, "My Family: %s.", string(data))
 }
