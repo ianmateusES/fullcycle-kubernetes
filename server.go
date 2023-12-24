6b009c6ef481b64953beb7870b661c76b1082b33
ianmateusES <estudo.ianmateus@gmail.com>
2023-12-24 01:44:28 -0300

    feat: creating livenessProbe

--- server.go
+++ server.go
@@ -6,12 +6,16 @@ import (
   "fmt"
   "io/ioutil"
   "log"
+  "time"
 )
+
+var startedAt = time.Now()
 
 func main() {
   http.HandleFunc("/", Hello)
   http.HandleFunc("/configmap", ConfigMap)
   http.HandleFunc("/secret", Secret)
+  http.HandleFunc("/healthz", Healthz)
 
   http.ListenAndServe(":8080", nil)
 }
@@ -37,4 +41,16 @@ func Secret(w http.ResponseWriter, r *http.Request) {
   password := os.Getenv("PASSWORD")
 
   fmt.Fprintf(w, "User: %s. Password: %s", user, password)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+  duration := time.Since(startedAt)
+
+  if duration.Seconds() < 10 {
+    w.WriteHeader(500)
+    w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+  } else {
+    w.WriteHeader(200)
+    w.Write([]byte("ok"))
+  }
 }
